f287bc86319c6a31046cf22554f4768fe7db383e
tuxudo <tuxudo@users.noreply.github.com>
2023-03-03 14:12:11 -0500

    Show if there is no data

--- views/sentinelone_client_tab.php
+++ views/sentinelone_client_tab.php
@@ -1,57 +1,60 @@
-    <h2 data-i18n="sentinelone.client_tab"></h2>
+<h2 data-i18n="sentinelone.client_tab"></h2>
 
-    <div id="sentinelone-msg" data-i18n="listing.loading" class="col-lg-12 text-center"></div>
+<div id="sentinelone-msg" data-i18n="listing.loading" class="col-lg-12 text-center"></div>
 
-    <div id="sentinelone-view" class="row hide">
-        <div class="col-md-6">
-            <table class="table table-striped">
-                <tr>
-                    <th data-i18n="sentinelone.threats_present"></th>
-                    <td id="sentinelone-active_threats_present"></td>
-                </tr>
-                <tr>
-                    <th data-i18n="sentinelone.agent_id"></th>
-                    <td id="sentinelone-agent_id"></td>
-                </tr>
-                <tr>
-                    <th data-i18n="sentinelone.agent_install_time"></th>
-                    <td id="sentinelone-agent_install_time"></td>
-                </tr>
-                <tr>
-                    <th data-i18n="sentinelone.agent_running"></th>
-                    <td id="sentinelone-agent_running"></td>
-                </tr>
-                <tr>
-                    <th data-i18n="sentinelone.agent_version"></th>
-                    <td id="sentinelone-agent_version"></td>
-                </tr>
-                <tr>
-                    <th data-i18n="sentinelone.enforcing_security"></th>
-                    <td id="sentinelone-enforcing_security"></td>
-                </tr>
-                <tr>
-                    <th data-i18n="sentinelone.last_seen"></th>
-                    <td id="sentinelone-last_seen"></td>
-                </tr>
-                <tr>
-                    <th data-i18n="sentinelone.mgmt_url"></th>
-                    <td id="sentinelone-mgmt_url"></td>
-                </tr>
-                <tr>
-                    <th data-i18n="sentinelone.self_protection_enabled"></th>
-                    <td id="sentinelone-self_protection_enabled"></td>
-                </tr>
-            </table>
-        </div>
-        <div class="col-md-6">
-        </div>
+<div id="sentinelone-view" class="row hide">
+    <div class="col-md-6">
+        <table class="table table-striped">
+            <tr>
+                <th data-i18n="sentinelone.threats_present"></th>
+                <td id="sentinelone-active_threats_present"></td>
+            </tr>
+            <tr>
+                <th data-i18n="sentinelone.agent_id"></th>
+                <td id="sentinelone-agent_id"></td>
+            </tr>
+            <tr>
+                <th data-i18n="sentinelone.agent_install_time"></th>
+                <td id="sentinelone-agent_install_time"></td>
+            </tr>
+            <tr>
+                <th data-i18n="sentinelone.agent_running"></th>
+                <td id="sentinelone-agent_running"></td>
+            </tr>
+            <tr>
+                <th data-i18n="sentinelone.agent_version"></th>
+                <td id="sentinelone-agent_version"></td>
+            </tr>
+            <tr>
+                <th data-i18n="sentinelone.enforcing_security"></th>
+                <td id="sentinelone-enforcing_security"></td>
+            </tr>
+            <tr>
+                <th data-i18n="sentinelone.last_seen"></th>
+                <td id="sentinelone-last_seen"></td>
+            </tr>
+            <tr>
+                <th data-i18n="sentinelone.mgmt_url"></th>
+                <td id="sentinelone-mgmt_url"></td>
+            </tr>
+            <tr>
+                <th data-i18n="sentinelone.self_protection_enabled"></th>
+                <td id="sentinelone-self_protection_enabled"></td>
+            </tr>
+        </table>
     </div>
+    <div class="col-md-6">
+    </div>
+</div>
 
 <script>
 $(document).on('appReady', function(e, lang) {
 
     // Get sentinelone data
     $.getJSON( appUrl + '/module/sentinelone/get_data/' + serialNumber, function( data ) {
+
+        // Only show if we have data
+        if (data && data.id !== ""){
             // Hide
             $('#sentinelone-msg').text('');
             $('#sentinelone-view').removeClass('hide');
@@ -105,8 +108,10 @@ $(document).on('appReady', function(e, lang) {
             } else{
                  $('#sentinelone-self_protection_enabled').text(data.self_protection_enabled);
             }
-            
-        });
+        } else {
+            $('#sentinelone-msg').text(i18n.t('no_data'));
+        }
+    });
 });
 
 </script>
